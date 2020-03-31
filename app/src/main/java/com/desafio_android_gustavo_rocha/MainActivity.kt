efbd33479731913f8d852ed82b8abef84f28a003
Gustavo <pereiragustavo215@gmail.com>
2020-03-31 20:19:15 -0300

    Initial commit

---
+++ app/src/main/java/com/desafio_android_gustavo_rocha/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.desafio_android_gustavo_rocha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
